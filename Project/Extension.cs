24c318a5f65d731c2ef13d7d0c3ae975852330ae
Hunanyan Davit <dav040201@gmail.com>
2022-10-10 20:46:23 +0400

    Add project files.

---
+++ Project/Extension.cs
@@ -0,0 +1,20 @@
+﻿
+static class Extension
+{
+    public static bool isPalindrome(this string str)
+    {
+       
+        str=str.Trim();
+        if (str.Length % 2 == 0)
+        {
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+        Console.WriteLine("Enter valid string:The Length must be even!!");
+        return false;
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+string str = "";
+Console.WriteLine("Enter string for palindrome(Exit for finish):");
+while (true)
+{
+    str = Console.ReadLine();
+    if (str.ToLower() == "exit") return;
+    Console.WriteLine(str.isPalindrome());
+}
+
